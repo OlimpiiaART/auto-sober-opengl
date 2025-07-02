f563536d5d32abeabc5f13154c2c28a5166a9c08
OlimpiiaART <101735973+OlimpiiaART@users.noreply.github.com>
2025-07-02 21:55:50 +0300

    Update sober_use_opengl.sh

--- sober_use_opengl.sh
+++ sober_use_opengl.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -euo pipefail
+# set -euo pipefail
 
 CONFIG_PATH="$HOME/.var/app/org.vinegarhq.Sober/config/sober/config.json"
 PLACEID_LIST="$HOME/.config/sober-opengl-placeids.txt"
@@ -16,22 +16,30 @@ PLACEID_LIST="$HOME/.config/sober-opengl-placeids.txt"
     exit 1
 }
 
-place_id=$(echo "${1##*placelauncherurl:}" | cut -d'+' -f1 | \
-    sed -e 's/%3A/:/g' -e 's/%2F/\//g' -e 's/%3D/=/g' -e 's/%26/\&/g' -e 's/%3F/?/g' | \
-    grep -oP 'placeId=\K[0-9]+')
+# echo "$@" > ~/txt
 
+place_id=$(echo "$1" | sed 's/%3D/=/g' | grep -oE 'placeId=[0-9]+' | sed 's/.*=//' || true)
 
 if [[ -z "$place_id" ]]; then
-    zenity --error --text="placeId not found"
-    exit 1
-fi
+    CHOICE=$(zenity --list --title="Use OpenGL?" --text="placeId not found, select OpenGL mode manually" --column="" "False" "True")
 
-if grep -qw "$place_id" "$PLACEID_LIST"; then
-    sed -i 's/"use_opengl": false/"use_opengl": true/' "$CONFIG_PATH"
-    #zenity --info --title="Sober OpenGL Toggle"  --text="✅ placeId $place_id found\nOpenGL enabled"
+    if [ -z "$CHOICE" ]; then
+        exit 0
+    fi
+
+    if [ "$CHOICE" = "True" ]; then
+        sed -i 's/"use_opengl": false/"use_opengl": true/' "$CONFIG_PATH"
+    elif [ "$CHOICE" = "False" ]; then
+        sed -i 's/"use_opengl": true/"use_opengl": false/' "$CONFIG_PATH"
+    fi
 else
-    sed -i 's/"use_opengl": true/"use_opengl": false/' "$CONFIG_PATH"
-    #zenity --info --title="Sober OpenGL Toggle" --text="⚠️ placeId $place_id not found\nOpenGL disabled"
+    if grep -qw "$place_id" "$PLACEID_LIST"; then
+        sed -i 's/"use_opengl": false/"use_opengl": true/' "$CONFIG_PATH"
+        # zenity --info --title="Sober OpenGL Toggle"  --text="✅ placeId $place_id found\nOpenGL enabled"
+    else
+        sed -i 's/"use_opengl": true/"use_opengl": false/' "$CONFIG_PATH"
+        # zenity --info --title="Sober OpenGL Toggle" --text="⚠️ placeId $place_id not found\nOpenGL disabled"
+    fi
 fi
 
 flatpak run org.vinegarhq.Sober "$@"
