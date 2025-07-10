7eac07f28a666ea33a7b4bdd056eb15c98309bcd
OlimpiiaART <101735973+OlimpiiaART@users.noreply.github.com>
2025-07-10 23:11:09 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -28,10 +28,9 @@ sudo cp "$DESKTOP_FILE" "/tmp/org.vinegarhq.Sober.desktop.backup.$(date +%Y%m%d%
 
 sudo sed -i "s|^Exec=.*|Exec=$INSTALL_PATH %u|" "$DESKTOP_FILE"
 
-# little useless
-# if command -v update-desktop-database >/dev/null; then
-#     sudo update-desktop-database /usr/share/applications/ 2>/dev/null || true
-#     sudo update-desktop-database ~/.local/share/applications/ 2>/dev/null || true
-# fi
+if command -v update-desktop-database >/dev/null; then
+    sudo update-desktop-database /usr/share/applications/ 2>/dev/null || true
+    sudo update-desktop-database ~/.local/share/applications/ 2>/dev/null || true
+fi
 
 echo "✅ The installation is complete. The script is installed in $INSTALL_PATH"
